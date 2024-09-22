4fe451bbe61c647d3b47c65459b1e23a31b679f3
Kelly Manson <klmanson@student.fullsail.edu>
2024-09-22 10:02:05 -0600

    first commit

---
+++ app.js
@@ -0,0 +1,26 @@
+require("dotenv").config();
+
+const http = require("http");
+const hostname = "127.0.0.1";
+const port = 3000;
+
+const fs = require("fs");
+let directory_name = "./";
+let filenames = fs.readdirSync(directory_name);
+
+const server = http.createServer((req, res) => {
+  res.statusCode = 200;
+  res.setHeader("Content-Type", "text/plain");
+
+  f = "";
+  filenames.forEach((file) => {
+    f += file + "\n";
+  });
+  res.end(f);
+});
+
+// Shared Secret Files
+const secret_key = "";
+console.log(`my secret password is ${secret_key}`);
+
+server.listen(port, hostname, () => {});
